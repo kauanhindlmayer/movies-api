1675f6ae4a29a3bb5bbec6aea980bf4e3f79522f
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-06-12 09:02:16 -0300

    keeping track of the endpoints

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mappings;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
@@ -7,30 +8,14 @@ using Movies.Contracts.Responses;
 namespace Movies.Api.Controllers;
 
 [ApiController]
-[Route("api")]
 public class MoviesController(IMovieRepository movieRepository) : ControllerBase
 {
-    [HttpPost("movies")]
+    [HttpPost(ApiEndpoints.Movies.Create)]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
-        var movie = new Movie
-        {
-            Id = Guid.NewGuid(),
-            Title = request.Title,
-            YearOfRelease = request.YearOfRelease,
-            Genres = request.Genres.ToList()
-        };
-        
+        var movie = request.MapToMovie();
         await movieRepository.CreateAsync(movie);
-        
-        var movieResponse = new MovieResponse
-        {
-            Id = movie.Id,
-            Title = movie.Title,
-            YearOfRelease = movie.YearOfRelease,
-            Genres = movie.Genres
-        };
-
-        return Created($"api/movies/{movie.Id}", movieResponse);
+        var movieResponse = movie.MapToResponse();
+        return Created(ApiEndpoints.Movies.Get.Replace("{id}", movie.Id.ToString()), movieResponse);
     }
 }
