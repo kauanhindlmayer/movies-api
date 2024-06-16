b16a9b86c190b76bb6a0f5327eb0709e260b6423
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-06-16 18:19:04 -0300

    introducing refit

---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,23 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+
+namespace Movies.Api.Sdk;
+
+public interface IMoviesApi
+{
+    [Get(ApiEndpoints.Movies.Get)]
+    Task<MovieResponse> GetMovieAsync(string idOrSlug);
+    
+    [Get(ApiEndpoints.Movies.GetAll)]
+    Task<IEnumerable<MovieResponse>> GetAllMoviesAsync(GetAllMoviesRequest request);
+    
+    [Post(ApiEndpoints.Movies.Create)]
+    Task<MovieResponse> CreateMovieAsync([Body] CreateMovieRequest request);
+    
+    [Put(ApiEndpoints.Movies.Update)]
+    Task<MovieResponse> UpdateMovieAsync(Guid id, [Body] UpdateMovieRequest request);
+    
+    [Delete(ApiEndpoints.Movies.Delete)]
+    Task DeleteMovieAsync(Guid id);
+}
